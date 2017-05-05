93134ca86a3c7950b89e18720c899bb66b603d05
Marek.Cervak <marek.cervak@seznam.cz>
2017-05-05 09:45:13 +0200

    pokec import

--- src/main/scala/cz.cvut.fit/gremlin/pokec/PokecImporter.java
+++ src/main/scala/cz.cvut.fit/gremlin/pokec/PokecImporter.java
@@ -1,5 +1,9 @@
 package cz.cvut.fit.gremlin.pokec;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.tinkerpop.gremlin.structure.Graph;
@@ -18,25 +22,25 @@ import java.util.stream.Stream;
  * @author Marek.Cervak
  */
 public class PokecImporter {
-    public static final List<String> COLUMN_NAMES = Arrays.asList("id", "public", "completion_percentage",	"gender", "region", "last_login",	"registration",	"age", "body", "I_am_working_in_field", "spoken_languages",	"hobbies",
-            "I_most_enjoy_good_food",	"pets",	"body_type", "my_eyesight",	"eye_color",	"hair_color",	"hair_type",	"completed_level_of_education",	"favourite_color",
-            "relation_to_smoking",	"relation_to_alcohol",	"sign_in_zodiac",	"on_pokec_i_am_looking_for",	"love_is_for_me",	"relation_to_casual_sex",	"my_partner_should_be",
-            "marital_status	children",	"relation_to_children",	"I_like_movies",	"I_like_watching_movie",	"I_like_music",	"I_mostly_like_listening_to_music",	"the_idea_of_good_evening",
-            "I_like_specialties_from_kitchen",	"fun",	"I_am_going_to_concerts",	"my_active_sports",	"my_passive_sports",	"profession",	"I_like_books",	"life_style",	"music",	"cars",
-            "politics",	"relationships",	"art_culture",	"hobbies_interests",	"science_technologies",	"computers_internet", "education",	"sport",	"movies",	"travelling",	"health",
-            "companies_brands",	"more");
+    public static final List<String> COLUMN_NAMES = Arrays.asList("id", "public", "completion_percentage", "gender", "region", "last_login", "registration", "age", "body", "I_am_working_in_field", "spoken_languages", "hobbies",
+            "I_most_enjoy_good_food", "pets", "body_type", "my_eyesight", "eye_color", "hair_color", "hair_type", "completed_level_of_education", "favourite_color",
+            "relation_to_smoking", "relation_to_alcohol", "sign_in_zodiac", "on_pokec_i_am_looking_for", "love_is_for_me", "relation_to_casual_sex", "my_partner_should_be",
+            "marital_status	children", "relation_to_children", "I_like_movies", "I_like_watching_movie", "I_like_music", "I_mostly_like_listening_to_music", "the_idea_of_good_evening",
+            "I_like_specialties_from_kitchen", "fun", "I_am_going_to_concerts", "my_active_sports", "my_passive_sports", "profession", "I_like_books", "life_style", "music", "cars",
+            "politics", "relationships", "art_culture", "hobbies_interests", "science_technologies", "computers_internet", "education", "sport", "movies", "travelling", "health",
+            "companies_brands", "more");
 
     static List<Integer> toStore = Arrays.asList(0, 3, 4, 7);
-    static Map <Integer, String> INDEXED_COLUMNS = toStore.stream().collect(Collectors.toMap(Function.identity(), c -> COLUMN_NAMES.get(c)));
+    static Map<Integer, String> INDEXED_COLUMNS = toStore.stream().collect(Collectors.toMap(Function.identity(), c -> COLUMN_NAMES.get(c)));
 
 
-    public static List<Map<String, String>> processRecords(File inputFile) throws IOException {
+    public static List<Map<String, String>> readRecords(File inputFile) throws IOException {
         List<Map<String, String>> ret = new ArrayList<>();
         LineIterator it = FileUtils.lineIterator(inputFile, "UTF-8");
         try {
             while (it.hasNext()) {
                 String line = it.nextLine();
-                ret.add(createRecord(line));
+                ret.add(createVertexRecord(line));
             }
         } finally {
             LineIterator.closeQuietly(it);
@@ -44,9 +48,45 @@ public class PokecImporter {
         return ret;
     }
 
-    public static Map<String, String> createRecord(String recordLine) {
-        Map<String,String> ret = new HashMap<>();
-        String [] records = recordLine.split("\\t");
+    public static Map<String, Record> readAndStoreVerticesRecords(File inputFile, Graph graph) throws IOException {
+        return readAndStoreRecords(inputFile, graph, true);
+    }
+
+    public static Map<String, Record> readAndStoreEdgeRecords(File inputFile, Graph graph) throws IOException {
+        return readAndStoreRecords(inputFile, graph, true);
+    }
+
+
+    private static Map<String, Record> readAndStoreRecords(File inputFile, Graph graph, boolean vertex) throws IOException {
+        Map<String, Record> ret = new HashMap<>();
+        LineIterator it = FileUtils.lineIterator(inputFile, "UTF-8");
+        try {
+            while (it.hasNext()) {
+                String line = it.nextLine();
+                if(vertex) {
+                    Record record = loadVertexToGraph(graph, createVertexRecord(line));
+                    ret.put(record.getId(), record);
+                }
+                else {
+                    loadEdgeToGraph(graph, line.split("\\t"));
+                }
+            }
+        } finally {
+            LineIterator.closeQuietly(it);
+        }
+        return ret;
+    }
+
+    private static void loadEdgeToGraph(Graph graph, String[] split) {
+        assert (split.length == 2);
+        Vertex to = graph.vertices(split[0]).next();
+        Vertex from = graph.vertices(split[1]).next();
+        from.addEdge("likes", to);
+    }
+
+    public static Map<String, String> createVertexRecord(String recordLine) {
+        Map<String, String> ret = new HashMap<>();
+        String[] records = recordLine.split("\\t");
         for (int i = 0; i < records.length; i++) {
             String columnName = INDEXED_COLUMNS.get(i);
             if (columnName != null) {
@@ -61,15 +101,13 @@ public class PokecImporter {
         return new Record(record.get("id"), vertex.id());
     }
 
-
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Record {
-        private final String id;
-        private final Object databaseId;
-
-        public Record(String id, Object databaseId) {
-            this.id = id;
-            this.databaseId = databaseId;
-        }
+        private String id;
+        private Object databaseId;
     }
 
 }
--- src/test/scala/cz.cvut.fit/gremlin/pokec/PokecImporterTest.java
+++ src/test/scala/cz.cvut.fit/gremlin/pokec/PokecImporterTest.java
@@ -1,11 +1,18 @@
 package cz.cvut.fit.gremlin.pokec;
 
+import cz.cvut.fit.gremlin.sources.TestSourceProvider;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 import org.apache.tinkerpop.gremlin.util.iterator.IteratorUtils;
+import org.apache.tinkerpop.shaded.kryo.Kryo;
+import org.apache.tinkerpop.shaded.kryo.io.Input;
+import org.apache.tinkerpop.shaded.kryo.io.Output;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +30,7 @@ public class PokecImporterTest {
     public void processRecords() throws Exception {
         File toProcess = new File(SRC_TEST_RESOURCES_POKEC + "profiles-" + SIZE + ".txt");
         assert(toProcess.exists());
-        List<Map<String,String>> records = PokecImporter.processRecords(toProcess);
+        List<Map<String,String>> records = PokecImporter.readRecords(toProcess);
         assert (records.size() == SIZE);
     }
 
@@ -31,13 +38,43 @@ public class PokecImporterTest {
     public void fullLoad() throws Exception {
         File toProcess = new File(SRC_TEST_RESOURCES_POKEC + "profiles-" + SIZE + ".txt");
         assert(toProcess.exists());
-        List<Map<String,String>> records = PokecImporter.processRecords(toProcess);
+        List<Map<String,String>> records = PokecImporter.readRecords(toProcess);
         Graph graph = TinkerGraph.open();
+        List<PokecImporter.Record> ids = new ArrayList<>();
         for (Map<String,String> record : records) {
-            PokecImporter.loadVertexToGraph(graph, record);
+             ids.add(PokecImporter.loadVertexToGraph(graph, record));
         }
         assert(IteratorUtils.count(graph.vertices()) == SIZE);
         graph.close();
+    }
+
+    @Test
+    public void fullLoadAndStoreIds() throws Exception {
+        TestSourceProvider.GraphSource graphSource = new TestSourceProvider.GraphInMemorySource("src/test/resources/orientDb-inmemory.properties");
+        graphSource.initGraph();
+        Graph graph = graphSource.getGraph();
+        File toProcess = new File(SRC_TEST_RESOURCES_POKEC + "profiles-" + SIZE + ".txt");
+        assert(toProcess.exists());
+        Map<String, PokecImporter.Record> records = PokecImporter.readAndStoreVerticesRecords(toProcess, graphSource.getGraph());
+        assert(IteratorUtils.count(graph.vertices()) == SIZE);
+        graphSource.clean();
+    }
+
+    @Test
+    public void testKryo() throws Exception {
+        List<PokecImporter.Record> ids = new ArrayList<>();
+        ids.add(new PokecImporter.Record("1", "1.1"));
+        ids.add(new PokecImporter.Record("2", "1.2"));
+        ids.add(new PokecImporter.Record("3", "1.3"));
+        Kryo k1 = new Kryo();
+        Output output = new Output(new FileOutputStream(SRC_TEST_RESOURCES_POKEC + "filename.ser"));
+        k1.writeObject(output, ids);
+        output.close();
+
+        Kryo k2 = new Kryo();
+        Input listRead = new Input(new FileInputStream(SRC_TEST_RESOURCES_POKEC + "filename.ser"));
+        List<PokecImporter.Record> restored = (List<PokecImporter.Record>) k2.readObject(listRead,  ArrayList.class);
+        assert (restored.size() == 3);
     }
 
 
